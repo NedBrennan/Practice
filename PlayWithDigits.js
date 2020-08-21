f5c73db219df7fe80883b89b8cea4deab08cc8ce
NedBrennan <nedbrnnn@gmail.com>
2020-08-21 18:18:09 -0400

    Full working solution

--- PlayWithDigits.js
+++ PlayWithDigits.js
@@ -1,13 +1,10 @@
-var test1 = 89
-var test2 = 92
-
 function digPow(n, p) {
     // create an array of all digits of n
     var nArray = Array.from(n.toString()).map(Number)
     power = p
+    num = n
     sum = 0
     k = 0
-    
     // Iterates over items in nArray and raises them
     // to succesivly larger powers
     for (i = 0; i < nArray.length; i ++) {
@@ -15,19 +12,33 @@ function digPow(n, p) {
         sum = sum + nArray[i]
         power += 1
         }
-
     // Checks for values of k to satisfy parameters
-    while (sum <= n) {
-        if (sum * k < n) {k = k + 1}
-        else if (sum * k == n) {
-            console.log("Yes " + k)
-            break
+    if (sum > num) {
+        while (sum >= num + 1) {
+            if (num * k < sum) {
+                k = k + 1
+            }
+            else if (num * k == sum) {
+                return k
+            }
+            else if (num * k > sum) {
+                return -1
+                break
+            } 
         }
-        else if (sum * k > n) {
-            console.log("No match found") 
-            break}
     }
+    else if (sum < num) {
+        while (sum <= num + 1) {
+            if (num * k < sum) {
+                k = k + 1
+            }
+            else if (num * k == sum) {
+                return k
+            }
+            else if (num * k > sum) {
+                return -1
+            }
+        }
+    }
+    else return 1
 }
-
-digPow(test1, 1)
-digPow(test2, 2)
