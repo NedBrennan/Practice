54222a862c55218defdb350eb9e7940fbbc2ad38
NedBrennan <nedbrnnn@gmail.com>
2020-08-21 22:58:46 -0400

    First Commit

---
+++ ternary.js
@@ -0,0 +1,12 @@
+// This is the Ternary operator
+// it operates like an if else statement without
+// the need for the words
+
+firstName = 'John'
+age = 16
+
+age >= 18 ? console.log(firstName + ' drinks a beer.') : console.log(firstName + ' drinks juice.');
+
+var drink = age >= 18 ? 'beer' : 'juice'
+console.log(drink);var job = 'teacher'
+switch (job) {
